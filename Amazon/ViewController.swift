604f850614f7ac13b20aa9f0244b055af12c7a25
ikrrish <kishangovindiya1@gmail.com>
2023-04-13 11:13:09 +0530

    Initial Commit

---
+++ Amazon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amazon
+//
+//  Created by R86 on 13/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
