08bce6a63c7835779bbcd1949e8360d0eaf45ee6
ikrrish <kishangovindiya1@gmail.com>
2023-04-19 14:35:57 +0530

    manthan

--- Amazon/Main Page.swift
+++ Amazon/Main Page.swift
@@ -18,8 +18,8 @@ class Home_page: UIViewController,UICollectionViewDelegate,UICollectionViewDataS
     var arrForImage1 = [1,2,3,4,5,6,7,8,9,10,11,12,13]
     var arrForAddImages = [17,18]
     var arrForHeadings = ["Deals","Electronics","Mobiles","Travel","miniTV","Home","Beauties","Furnitures","Pharmacy","Movies","Books,Toys","Appliances","Grocery"]
-    var arrFor2ngAdd = ["Amazon Pay","Scan any QR","Send Money","Pay Bills","Keep shopping for"]
-    var arrForImage2ndAdd = [UIImage(named: "21") ,UIImage(systemName: "qrcode"),UIImage(systemName: "newspaper.fill"),UIImage(systemName: "indianrupeesign.square"),UIImage(named: "22")]
+    var arrFor2ngAdd = ["Amazon Pay","Scan any QR","Pay Bills","Send Money","Keep shopping for",""]
+    var arrForImage2ndAdd = [UIImage(named: "21") ,UIImage(systemName: "qrcode"),UIImage(systemName: "newspaper.fill"),UIImage(systemName: "indianrupeesign.square"),UIImage(named: "22"),UIImage(named: "")]
     override func viewDidLoad() {
         super.viewDidLoad()
         lable.layer.cornerRadius = 11
@@ -44,7 +44,8 @@ class Home_page: UIViewController,UICollectionViewDelegate,UICollectionViewDataS
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == self.collectionView1 {
             let cell1 = collectionView1.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath) as! CollectionViewCellForTopHeaders
-            
+          
+           
             cell1.image1.image = UIImage(named: "\(arrForImage1[indexPath.row])")
             cell1.lable1.text = arrForHeadings[indexPath.row]
             return cell1
@@ -52,14 +53,16 @@ class Home_page: UIViewController,UICollectionViewDelegate,UICollectionViewDataS
         
         else if collectionView == self.collectionView2{
             let cell2 = collectionView2.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath) as! CollectionViewCellForMidAdvertise
-            
+            cell2.backgroundColor = UIColor.white
             cell2.imageForAdd.image = UIImage(named: "\(arrForAddImages[indexPath.row])")
             return cell2
         }
         
         else {
             let cell3 = collectionView3.dequeueReusableCell(withReuseIdentifier: "cell3", for: indexPath) as! CollectionViewCellFor2ndAdd
-          
+            cell3.backgroundColor = UIColor.white
+            cell3.backgroundColor = UIColor.white
+            cell3.layer.cornerRadius = 11
             cell3.lableFor2ndAdd.text = arrFor2ngAdd[indexPath.row]
             cell3.iamgeFor2ndAdd.image = arrForImage2ndAdd[indexPath.row]
             
@@ -77,7 +80,7 @@ class Home_page: UIViewController,UICollectionViewDelegate,UICollectionViewDataS
             return CGSize(width: 425, height: 265)
         }
         else {
-            return CGSize(width: 150, height: 200)
+            return CGSize(width: 145, height: 175)
         }
     }
 }
